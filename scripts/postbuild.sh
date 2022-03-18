22d8a3b9b319b5cd061e474c7140063938611c65
Maros Hluska <mhluska@gmail.com>
2022-03-18 01:28:59 -0700

    Add index.html to deploy

---
+++ scripts/postbuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e # If anything fails, stop the script.
+set -u # If an undefined variable is expanded, stop the script.
+
+ROOT=$(git rev-parse --show-toplevel)
+
+cd "${ROOT}"
+
+rsync -rta postbuild/ build
+cp index.html build/
